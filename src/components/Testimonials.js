6bf602fd51622c1ec3deca1929112d56f7ed6ecb
Pridhviraj <pridhvi02@gmail.com>
2024-08-15 00:10:48 +0530

    added inspiration

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -6,7 +6,7 @@ const Testimonials = ({ resumeData }) => {
       <div className="text-container">
         <div className="row">
           <div className="two columns header-col">
-            <h1><span>Client Testimonials</span></h1>
+            <h1><span>Inspiration</span></h1>
           </div>
           <div className="ten columns flex-container">
             <div className="flexslider">
